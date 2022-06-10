b161ac495fd4d8a7660f8e098d44cc791182eae2
MAC BOOK PRO <inyounghwang.lindsey@gmail.com>
2022-06-10 21:13:01 +0900

    create a task to extract data from csv file

--- csv2db.sh
+++ csv2db.sh
@@ -4,3 +4,11 @@
 
 # Loads the data from the CSV file into a table in PostgreSQL database.
 
+# Extract phase
+
+echo "Extracting data"
+
+# Extract the columns 
+# 1:rowid, 2: timestamp, 3: anaoymized_vehicle_number, 4: vehicle_type
+
+cut -d"," -f1-4 vehicle-data.csv > csv_data.csv
