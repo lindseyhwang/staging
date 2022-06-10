f3ef665cc7465194255c7c8d7893c9bb5ffb7fb1
MAC BOOK PRO <inyounghwang.lindsey@gmail.com>
2022-06-10 22:57:59 +0900

    update conslidate shell

---
+++ consolidate.sh
@@ -0,0 +1,12 @@
+# This script
+# Combines all the data.
+# The fields : Rowid, Timestamp, Anonymized Vehicle number, Vehicle type, Number of axles, 
+# Tollplaza id, Tollplaza code, Type of Payment code, and Vehicle Code
+
+# Loads the data from the CSV file into a table in PostgreSQL database.
+
+# Combine phase
+
+echo "Combining data"
+
+paste csv_data.csv tsv_data.csv fixed_width_data.csv > extracted_data.csv  
