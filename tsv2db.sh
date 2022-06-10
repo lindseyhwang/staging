ea0a96f7686d7e469430c20ab45daff5f8677bca
MAC BOOK PRO <inyounghwang.lindsey@gmail.com>
2022-06-10 22:30:29 +0900

    update tsv2db.sh

---
+++ tsv2db.sh
@@ -0,0 +1,14 @@
+# This script
+# Extracts data from tollplaza-data.tsv file into a CSV file.
+# The fields : Number of axles, Tollplaza id, and Tollplaza code
+
+# Loads the data from the CSV file into a table in PostgreSQL database.
+
+# Extract phase
+
+echo "Extracting data"
+
+# Extract the columns 
+# 5:numb_of_axles, 6: tollplaza_id, 7: tollplaza_code
+
+cut -f5,6,7 tollplaza-data.tsv > tsv_data.csv
